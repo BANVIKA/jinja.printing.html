afdb5adc142fb09a0fea19dcbf33812cb7618780
BANVIKA <achannagariharshini99@gmail.com>
2023-07-28 12:47:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def data_render(request):
     d={'name':'harshini','age':23}
     return render(request,'data_render.html',context=d)
+
+
+def condition(request):
+    d={'a':1000,'b':16}
+    return render(request,'condition.html',context=d)
